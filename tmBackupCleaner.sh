83d97e5a0d124adb145cdf5bdbdee25b0ceb986d
tr1kn1 <35529876+tr1kn1@users.noreply.github.com>
2020-11-18 14:49:19 +0100

    updated to only keep latest backup

--- tmBackupCleaner.sh
+++ tmBackupCleaner.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 latest=$(tmutil latestbackup)
-year="2020"
 tmutil listbackups | while read backup; do
-    if [[ "$backup" != "$latest" ]] && [[ "$backup" != *"${year}-08"* ]] && [[ "$backup" != *"$year"-06* ]] && [[ "$backup" != *"$year"-03* ]] && [[ "$backup" != *"$year"-02* ]]; then
-        echo tmutil delete "$backup"
+    if [[ "$backup" != "$latest" ]]; then
+        tmutil delete "$backup"
     fi
 done
