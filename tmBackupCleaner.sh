5d329997a7cbf3886691f0a230e1f0c9577b4c2c
tr1kn1 <35529876+tr1kn1@users.noreply.github.com>
2021-01-17 11:44:30 +0100

    further adaptions to new api

--- tmBackupCleaner.sh
+++ tmBackupCleaner.sh
@@ -2,7 +2,10 @@
 pathToLatest=$(tmutil latestbackup)
 backupDir=`dirname $pathToLatest`
 latestBackup=`basename $pathToLatest`
+hardDriveMountPoint=`echo $backupDir | sed 's/\/Backups\.backupdb.*//g'`
 
+
+echo -e "The mount point for the connected TimeMachine drive:\n$hardDriveMountPoint\n"
 echo -e "Path to backups on the connected TimeMachine device:\n$backupDir\n"
 echo -e "List of available backups:\n$(tmutil listbackups)\n"
 echo -e "Latest backup, which is not deleted:\n$latestBackup\n"
@@ -10,7 +13,8 @@ echo -e "Start deleting older backups, this can take a lot of time."
 
 tmutil listbackups | while read backup; do
     if [[ "$backup" != "$latestBackup" ]]; then
-        echo tmutil delete "$backupDir/$backup"
+        # Remove 'echo' on the next line only when the output of the dry run is correct!
+        echo tmutil delete -d "$hardDriveMountPoint" -t "$backup"
     fi
 done
 
