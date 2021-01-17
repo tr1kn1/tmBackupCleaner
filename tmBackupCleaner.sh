f2ea384930689c032f4e988cf32514b8ec41988d
tr1kn1 <35529876+tr1kn1@users.noreply.github.com>
2021-01-17 10:53:25 +0100

    added code for the tmutil api changes in Big Sur

--- tmBackupCleaner.sh
+++ tmBackupCleaner.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-latest=$(tmutil latestbackup)
+pathToLatest=$(tmutil latestbackup)
+backupDir=`dirname $pathToLatest`
+latestBackup=`basename $pathToLatest`
+# code before Big Sur
+#$(tmutil latestbackup)
 tmutil listbackups | while read backup; do
-    if [[ "$backup" != "$latest" ]]; then
-        tmutil delete "$backup"
+    if [[ "$backup" != "$latestBackup" ]]; then
+        # Big Sur workaroung
+        #echo tmutil delete -d /Volumes/TimeMachineWD -t "$backup"
+        echo tmutil delete "$backupDir/$backup"
     fi
 done
